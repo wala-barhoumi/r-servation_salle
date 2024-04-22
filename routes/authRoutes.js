a8a21dc8dc7df7cc663ed7a28acc267a93ebb2e6
wala-barhoumi <walabarhoumi081@gmail.com>
2024-04-22 14:57:06 +0100

    nav

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
-// Routes for login
 router.get('/login', authController.getLogin);
 router.post('/login', authController.login);
 
@@ -13,7 +12,7 @@ router.post('/signup', authController.register);
 // Route for logout
 router.get('/logout', authController.logout);
 
-//listuser
+
 
 router.get('/userList', authController.getUserList);
 module.exports = router;
